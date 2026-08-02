a02bf75fc38645051472e95dee36cbf1e00e4d1b
Dipa-Sarker <dipasarker696@gmail.com>
2026-08-02 21:29:42 +0100

    style with CSS

--- script.js
+++ script.js
@@ -7,20 +7,26 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  
-  for(const episode of episodeList){
-  const card = document.createElement("section");
-  card.classList.add("episode-card");
-  const episodeName = document.createElement("h2");
-episodeName.textContent = episode.name + "-" + "S" + episode.season.toString().padStart(2, "0") + "E" + episode.number.toString().padStart(2, "0");
-card.append(episodeName);
-const image = document.createElement("img");
-image.src = episode.image.medium;
-card.append(image);
-const summary = document.createElement("p");
-summary.innerHTML = episode.summary;
-card.append(summary);
-  rootElem.append(card);
+
+  for (const episode of episodeList) {
+    const card = document.createElement("section");
+    card.classList.add("episode-card");
+    const episodeName = document.createElement("h2");
+    episodeName.textContent =
+      episode.name +
+      "-" +
+      "S" +
+      episode.season.toString().padStart(2, "0") +
+      "E" +
+      episode.number.toString().padStart(2, "0");
+    card.append(episodeName);
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    card.append(image);
+    const summary = document.createElement("p");
+    summary.innerHTML = episode.summary;
+    card.append(summary);
+    rootElem.append(card);
   }
 }
 
