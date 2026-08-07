947a55520a30b167cde89e600af08c67804fded5
Dagim-Daniel <dagimdan19@gmail.com>
2026-08-07 14:33:35 +0100

    refactoring  the level 100 code you used

---
+++ code-comparison.md
@@ -0,0 +1,26 @@
+#level 100 comparison
+
+## How is it different?
+
+i used to manage the page using template tag and also i access it through querySelector().
+i used separator for the season and episode numbers. and you used border for the episode title
+the way we use to show the episode title including the season and episode number, i used episodeCode to show it.
+i used map() for looping
+
+## What do you prefer about your implementation?
+
+i prefer my template tag and also i used comma to separate Season and Episode numbers. but also like yours too.
+i used template string for Episode title where as you used concatenation method.
+i prefer my way of using episodeCode and to show episodeTitle.
+used map() for looping
+
+## What do you prefer about their implementation?
+
+how you use the original link.
+how to append different element to the DOM.
+created card element manually
+used for of loop creating card for all episodes.
+
+## What did you learn that you didn't know before?
+
+how to append everything but when tried to refactor i got that we use one append to append different element to the DOM
--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  console.log(allEpisodes);
+
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-
   for (const episode of episodeList) {
     const card = document.createElement("section");
     card.classList.add("episode-card");
-    const episodeName = document.createElement("h2");
-    episodeName.textContent =
-      episode.name +
-      "-" +
-      "S" +
-      episode.season.toString().padStart(2, "0") +
-      "E" +
-      episode.number.toString().padStart(2, "0");
-    card.append(episodeName);
+
+    //this could be done using template literals, but I wanted to show you a different way to do it
+    const seasonNumber = episode.season.toString().padStart(2, "0");
+    const episodeNumber = episode.number.toString().padStart(2, "0");
+    const episodeCode = `S${seasonNumber},E${episodeNumber}`;
+
+    //change episodeName to episodeInfo
+    const episodeInfo = document.createElement("episodeTitle");
+    episodeInfo.textContent = `${episode.name}-${episodeCode}`;
+
+    // summary is here
+    const summary = document.createElement("description");
+    summary.innerHTML = episode.summary;
+
+    //image is here
     const image = document.createElement("img");
     image.src = episode.image.medium;
-    image.alt = `Episode image for ${episode.name}`; 
-    card.append(image);
-    const summary = document.createElement("p");
-    summary.innerHTML = episode.summary;
-    card.append(summary);
+    image.alt = `Episode image for ${episode.name}`;
+
+    card.append(episodeInfo, image, summary);
     rootElem.append(card);
   }
 }
-console.log("hello");
 
 window.onload = setup;
